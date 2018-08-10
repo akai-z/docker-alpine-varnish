646251cb55499727d62570a5b1136c9b8845b6ec
akai-z <akai-z@users.noreply.github.com>
2018-08-11 02:27:22 +0300

    Add environment variable VSL_RECLEN

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,7 +8,7 @@ varnishd -a :$WEB_SERVER_PORT \
   -T $VARNISH_ADMIN_LISTEN_ADDRESS:$VARNISH_ADMIN_LISTEN_PORT \
   -Ff /etc/varnish/default.vcl \
   -p feature=+esi_disable_xml_check,+esi_ignore_other_elements \
-  -p vsl_reclen=4084 \
+  -p vsl_reclen=$VSL_RECLEN \
   -p vcc_allow_inline_c=on \
   -p 'cc_command=exec cc -fpic -shared -Wl,-x -o %o %s -lcrypto -lssl' \
   -s malloc,$MALLOC
