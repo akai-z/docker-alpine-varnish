d18db321596810a38e37bf40edc712c1f178dee7
akai-z <akai-z@users.noreply.github.com>
2018-08-11 20:44:44 +0300

    Rename some environment variables

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 envsubst < /etc/varnish/default.vcl.template > /etc/varnish/default.vcl
 
 varnishd -a $VARNISH_LISTEN_ADDRESS:$VARNISH_LISTEN_PORT \
-  -T $VARNISH_ADMIN_LISTEN_ADDRESS:$VARNISH_ADMIN_LISTEN_PORT \
+  -T $VARNISH_MANAGEMENT_LISTEN_ADDRESS:$VARNISH_MANAGEMENT_LISTEN_PORT \
   -Ff /etc/varnish/default.vcl \
   -p feature=+esi_disable_xml_check,+esi_ignore_other_elements \
   -p vsl_reclen=$VSL_RECLEN \
